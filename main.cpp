d0a961529869fa882e44fa4abecb074869ea7565
JLeipus <51248052+jleipus@users.noreply.github.com>
2020-07-01 11:31:22 +0300

    Update main.cpp
    Size and speed are passed as console arguments, defaults are set to 10 and 10.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    Game g;
-
+	// initial board size and tickspeed are passed as console arguments
+	// default values are 10 and 10 ticks/s
+    if(argc > 1) {
+		Game g(atoi(argv[1]), atoi(argv[2]));
+	} else {
+		Game g(10, 10);
+	}
     return 0;
 }
